0389e18559a53c2c42bbad83eaacf24a760a0017
zuazhang <905753474@qq.com>
2017-11-15 20:48:23 +0800

    py

--- Python/PYtest/re.py
+++ Python/PYtest/re.py
@@ -27,3 +27,46 @@ else:
 	print "no match"
 
 
+
+#re.search方法
+matchObj = re.search(r'(.*) are (.*?) .*',line,re.M|re.I)
+
+if matchObj:
+	print "matchObj.group() : ",matchObj.group()
+	print "matchObj.group(1) : ",matchObj.group(1)
+	print "matchObj.group(2) : ",matchObj.group(2)
+else:
+	print "no match"
+
+#re.match 与 re.search 的区别
+'''
+re.match只匹配字符串的开始，如果字符串开始不符合正则表达式，匹配失败。
+函数返回none；而re.search匹配整个字符串，知道找到一个匹配
+
+'''	
+matchObj = re.match(r'dogs',line,re.M|re.I)
+if matchObj:
+	print "match------->matchObj.group():",matchObj.group()
+else:
+	print "no match!!!!"
+matchObj = re.search('dogs',line,re.M|re.I)
+if matchObj:
+	print "search------>matchObj.group():",matchObj.group()
+else:
+	print "no search!!!!"
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	

5ce17383f2b27162a8ea2d0a7fa2c8d3746c3e64
zuazhang <905753474@qq.com>
2017-11-17 16:55:31 +0800

    py

---
+++ Python/PYtest/web.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import urllib,urllib2
+#获取网页源代码
+url = 'http://www.baidu.com'
+r = urllib.urlopen(url)
+#print r.read()
+#urllib.urlretrieve()下载方法
+urllib.urlretrieve('http://labfile.oss.aliyuncs.com/courses/761/w8ay_1.zip',filename='F:/git/demo1/Python/PYtest/baidu.zip')

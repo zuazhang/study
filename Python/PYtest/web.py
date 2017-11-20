4efc0c993417f2ae6a2d146f2f94d362d004f465
zuazhang <905753474@qq.com>
2017-11-20 17:40:58 +0800

    py

--- Python/PYtest/web.py
+++ Python/PYtest/web.py
@@ -3,6 +3,10 @@ import urllib,urllib2
 #获取网页源代码
 url = 'http://www.baidu.com'
 r = urllib.urlopen(url)
-#print r.read()
-#urllib.urlretrieve()下载方法
+print r.read()
+#urllib.urlretrieve()下载文件（属于urllib库）
 #urllib.urlretrieve('http://labfile.oss.aliyuncs.com/courses/761/w8ay_1.zip',filename='F:/git/demo1/Python/PYtest/baidu.zip')
+#urllib2.Requests()  定制请求头
+
+
+

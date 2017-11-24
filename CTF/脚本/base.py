92f9020e11bcc543e5f11bae278846f9a3a88176
zuazhang <905753474@qq.com>
2017-11-24 14:57:59 +0800

    py

--- CTF/脚本/base.py
+++ CTF/脚本/base.py
@@ -11,12 +11,21 @@ def encode(str):
 	print "base16 encode : "+c
 #base解密函数
 def decode(str):
-	a = base64.b64decode(str)
-	b = base64.b32decode(str)
-	c = base64.b16decode(str)
-	print "base64 decode : "+a
-	print "base32 decode : "+b
-	print "base16 decode : "+c
+	try:
+		a = base64.b64decode(str)
+		print "base64 decode : "+a+"\n"
+	except:
+		print "the string is not decoded by base_64\n"
+	try:
+		b = base64.b32decode(str)
+		print "base32 decode : "+b+"\n"
+	except:
+		print "the string is not decoded by base_32\n"
+	try:
+		c = base64.b16decode(str)
+		print "base16 decode : "+c+"\n"
+	except:
+		print "the string is not decoded by base_16\n"
 
 def choice_1():
 	s = str(raw_input("please input the string what you want to encode:"))
@@ -24,11 +33,16 @@ def choice_1():
 def choice_2():
 	s = str(raw_input("please input the Base_string what you want to decode:"))
 	decode(s)
-print "please choice the option :\n1.encode the String\n2.decode the Base_string\n"
-n = int(raw_input("please choice:"))
-if n == 1:
-	choice_1()
-elif n == 2:
-	choice_2()
-else:
-	print "Wrong !!!!\n please choice 1 or 2!!!!!"
+
+def main():
+	print "please choice the option :\n1.encode the String\n2.decode the Base_string\n"
+	n = int(raw_input("please choice:"))
+	if n == 1:
+		choice_1()
+	elif n == 2:
+		choice_2()
+	else:
+		print "Wrong !!!!\n please choice 1 or 2!!!!!"
+
+if __name__ == '__main__':
+	main()

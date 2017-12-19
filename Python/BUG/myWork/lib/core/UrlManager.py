7c44221a296cbda6cded0d6f372d89ee3297f599
zuazhang <905753474@qq.com>
2017-12-19 22:29:59 +0800

    py

---
+++ Python/BUG/myWork/lib/core/UrlManager.py
@@ -0,0 +1,28 @@
+# -*-coding: utf-8 -*-
+
+class UrlManager(object):
+	def __int__(self):
+		self.new_urls = set()
+		self.old_urls = set()
+
+	def add_new_url(self,url):
+		if url is None:
+			return 
+		if url not in self.new_urls adn url not in self.old_urls:
+			self.new_urls.add(url)
+
+	def add_new_urls(self,urls):
+		if urls in None or len(urls) == 0:
+			return
+		if url in urls:
+			self.add_new_url(url)
+
+	def has_new_url(slef):
+		return len(self.new_urls) != 0
+
+	def get_new_url(self):
+		new_url = self.new_urls.pop()
+		self.old_urls.add(new_url)
+		return new_url
+
+

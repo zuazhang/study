f4cfb6d518e9344aafb3f4beeb31f5271a3d8df3
zuazhang <905753474@qq.com>
2018-01-09 14:50:35 +0800

    duanzi

--- Python/PYtest/sublime/Spider/duanzi_spider.py
+++ Python/PYtest/sublime/Spider/duanzi_spider.py
@@ -18,10 +18,10 @@ class Spider:
 		@param page 需要请求的第几页
 		@returns  返回的html页面
 		'''
-		url = 'http://www.neihan8.com/article/list_5_'+str(page)+'.html'
+		url = 'http://www.neihan8.com/article/list_5_'+ str(page) +'.html'
 		user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT6.1; Trident/5.0'
 		headers = {'User-Agent' : user_agent}
-		req = urllib2.Request(url,headers = headers)
+		req = urllib2.Request(url, headers = headers)
 		response = urllib2.urlopen(req)
 		html = response.read()
 		#right
